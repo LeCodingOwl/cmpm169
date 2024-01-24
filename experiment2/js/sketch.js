0dbf93a9e457571bc4a41329f2d27ab63b2cb94b
LeCodingOwl <nguyenjimmy627@gmail.com>
2024-01-23 21:30:31 -0800

    Updated Sketch.js with code fromp5.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,69 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jimmy Nguyen
+// Date: 1/22/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
 // Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
+var colorCount = 20;
+var hueValues = [];
+var saturationValues = [];
+var brightnessValues = [];
+var actRandomSeed = 0;
+var alphaValue = 75;
 
 // setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
 }
-
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    loop();
+    background(0, 0, 54.5);
+    
+    // ------ colors ------
+    // create palette
+    for (var i = 0; i < colorCount; i++) {
+      if (i % 2 == 0) {
+        hueValues[i] = random(360);
+        saturationValues[i] = 100;
+        brightnessValues[i] = random(100);
+      } else {
+        hueValues[i] = 195;
+        saturationValues[i] = random(20);
+        brightnessValues[i] = 100;
+      }
+    }
+    
+    // --- area tiling ---
+    // --- draw lanterns ---
+    var counter = 0;
+    var rowCount = int(random(5, 30));
+    var rowHeight = height / rowCount;
+    
+    for ( var i = rowCount; i >= 0; i--) {
+      var x = random(0, width);
+      var y = random(0, height);
+      var w = random(5, 15);
+      var h = w * 3;
+      
+      var index = counter % colorCount;
+      var col1 = color(0, 0, 54.5);
+      var col2 = color(hueValues[index], saturationValues[index], brightnessValues[index], alphaValue);
+      gradient(x, y, w, h, col1, col2);
+      
+      counter++;
+    }
 }
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  function gradient(x, y, w, h, c1, c2) {
+    var ctx = drawingContext; // global canvas context p5.js var
+    var grd = ctx.createLinearGradient(x, y, x, y + h);
+    grd.addColorStop(0, c1.toString());
+    grd.addColorStop(1, c2.toString());
+    ctx.fillStyle = grd;
+    ctx.fillRect(x, y, w, h);
 }
