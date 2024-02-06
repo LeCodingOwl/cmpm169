587e403dbc749e8386a7d3742ca723c300c26abc
LeCodingOwl <nguyenjimmy627@gmail.com>
2024-02-06 15:40:37 -0800

    Added code, images. and sounds

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -7,61 +7,128 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
 
 // Globals
-let myInstance;
-let canvasContainer;
+let deck = [];
+let cardName = [];
+let stars = [];
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+let yPos;
+let targetY;
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
+let textOpacity = 0;
+let fadeInSpeed = 2;
+
+let flashAlpha = 0;
+
+let clicked = false;
+
+let charmSound;
+
+
+function preload() {
+    charmSound = loadSound('Sounds/rollingSound.mp3');
+  }
 
 // setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
+
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    for (let cards of ['Alhaitham', 'Ayato', 'Baizhu', 'Childe', 'Cyno', 'Diluc','Eula', 'Ganyu', 'Hu Tao', 'Itto', 'Jean', 'Kazuha', 'Keqing','Klee', 'Lyney', 'Nahida', 'Neuvillette', 'Raiden', 'Venti','Wanderer', 'Yae Miko', 'Zhongli']) 
+    {
+        deck.push(loadImage(`Tarot/${cards}.jpg`));
+        cardName.push(cards);
+    }
+  
+    yPos = height + 500;
+    targetY = height / 2;
+  
+    for (let i = 0; i < 100; i++) {
+    stars[i] = createStar();
+    }
+  
+    shuffleDeck();
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    background(0, 0, 54.5)
+  
+    flashAlpha -= 5;
+  
+    image(deck[0], width / 2 - deck[0].width / 2, yPos -  deck[0].height / 2);
+  
+    if (clicked) {
+      for (let star of stars) {
+        star.move();
+        star.display();
+        
+        textOpacity = min(textOpacity + fadeInSpeed, 255);
+        
+        fill(255, 255, 255, textOpacity);
+        stroke(0);
+        strokeWeight(4);
+        textSize(32);
+        textAlign( CENTER, CENTER);
+        
+        let centerX = width / 2;
+        let centerY = height /2 + 150;
+        
+        text(cardName[0], centerX, centerY);
+        
+      }
+      yPos = lerp(yPos, targetY, 0.1);
+    }
+    
+    
+    if (flashAlpha > 0) {
+      background(0, 0, 54.5, flashAlpha);
+    }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
-}
+    target = height / 2;
+    if (!clicked) {
+      flashAlpha = 255;
+      charmSound.play();
+    }  
+    clicked = true;
+  }
+  
+  function shuffleDeck() {
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = floor(random(i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+      [cardName[i], cardName[j]] = [cardName[j], cardName[i]];
+    }
+  }
+  
+  function createStar() {
+    return {
+      x: random(width),
+      y: random(height),
+      size: random(1, 5),
+      speed: random(1, 5),
+  
+      move: function() {
+        this.y += this.speed;
+        if (this.y > height) {
+          this.y = 0;
+          this.x = random(width);
+        }
+      },
+  
+      display: function() {
+        fill(255, 255, 0);
+        noStroke();
+        rect(this.x, this.y, this.size, this.size, 20);
+      }
+    };
+  }
