43369edcb7dd4cabcb57b259838a6fc2d04d4e73
LeCodingOwl <nguyenjimmy627@gmail.com>
2024-02-19 23:17:05 -0800

    Added code from p5.js, added updated index.html

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,28 +1,17 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jimmy Nguyen
+// Date: 2/19/24
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let inputText = "Hello World";
+let morseCode = "";
+let frameCounter = 0;
+let currentCharIndex = 0;
+let currentMorseIndex = 0;
+let delay = 0;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+    beep = loadSound('beep.mp3');
+    longerBeep = loadSound('longerBeep.mp3');
 }
 
 // setup() function is called once when the program starts
@@ -36,32 +25,15 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    textFont("Arial", 24);
+    textAlign(CENTER, CENTER);
+
+    // Convert input text to Morse code
+    morseCode = textToMorse(inputText);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
 }
