0ba1d1c84b8945105a2b75f91f6f4c9e0edbd4ca
LeCodingOwl <nguyenjimmy627@gmail.com>
2024-02-26 22:27:39 -0800

    added api

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -7,7 +7,7 @@ let covidData;
 function preload() {
   // Load the JSON data file before the sketch starts
   //covidData = loadJSON("https://covid.ourworldindata.org/data/owid-covid-data.json", "json");
-  covidData = loadTable('path/to/your/downloaded/file.csv', 'csv', 'header');
+  covidData = loadTable('api/owid-covid-data.csv', 'csv', 'header');
 }
 
 // setup() function is called once when the program starts
